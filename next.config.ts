2e7b0da09b50d3f1caaa2f81630f14532d63d061
Stephen Sequenzia <sequenzia@gmail.com>
2026-01-29 01:25:19 -0500

    Add allowedDevOrigins to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   // Turbopack is enabled by default in Next.js 15+ when using `next dev --turbopack`
+  allowedDevOrigins: [
+    '*',
+  ], 
 };
 
 export default nextConfig;
